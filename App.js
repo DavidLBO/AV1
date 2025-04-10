1e8ab64b13027186153c61b668ea6e357648450d
DavidLBO <davidlopes009@gmail.com>
2025-04-10 18:55:08 -0300

    Iniciando Projeto

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useState } from 'react'; 
+
+import inputField from './components/inputField';
+import buttonComponent from './components/buttonComponent';
+import contactList from './components/contactList';
 
 export default function App() {
+  const [nome, setNome] = useState('');
+  const [Telefone, setTelefone] = useState('');
+  const [lista, setLista] = useState([]);
+
+  function cadastrarDados(){
+    if(nome.trim() === '' || Telefone.trim() === '') return;
+    setLista([...lista, {nome, Telefone}]);
+    setNome('');
+    setTelefone('');
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView style = {{flex: 1}}>
+
+    <View style={styles.maiContainer}>
+        <Text style = {styles.Title}>Cadastro de Usuário</Text>
+
+        <inputField label="Nome:" value={nome} onChangeText={setNome} />
+        <inputField label="Telefone" value={Telefone} onChangeText={setTelefone} />
+
+        <buttonComponent title={'Cadastrar'} onPress={cadastrarDados}/>
+
+        <contactList lista={lista}/>
+
+        <StatusBar style="auto" />
     </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+export const styles = StyleSheet.create({
+  maiContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 20,
+    paddingTop: 40,
+    justifyContent: 'flex-start',
+  },
+  Title: {
+    padding: 10,
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
